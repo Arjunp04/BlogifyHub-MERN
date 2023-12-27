84cc6f84d211c92486e95bdf49c88a9110cde1ce
Arjunp04 <arjunprajapati4102001@gmail.com>
2023-12-27 13:30:38 +0530

    updated creatpost commit

--- frontend/src/Pages/CreatePost.jsx
+++ frontend/src/Pages/CreatePost.jsx
@@ -82,41 +82,41 @@ const CreatePost = () => {
             <Navbar />
             <div className='px-6 md:px-[200px] mt-8 '>
                 <div className='bg-paleYellow rounded-2xl px-8 py-8 border-4 border-gray-300 '>
-                <h1 className='font-bold md:text-2xl text-xl '>Create your Post</h1>
-                <form className='w-full flex flex-col space-y-4 md:space-y-8 mt-4'>
+                    <h1 className='font-bold md:text-2xl text-xl '>Create your Post</h1>
+                    <form className='w-full flex flex-col space-y-4 md:space-y-8 mt-4'>
 
-                    <input onChange={ ( e ) => setTitle( e.target.value ) } type="text" placeholder='Enter post title' className='px-4 py-2   rounded-full border-2'/>
+                        <input onChange={ ( e ) => setTitle( e.target.value ) } type="text" placeholder='Enter post title' className='px-4 py-2   rounded-full border-2' />
 
-                    <input onChange={ ( e ) => setFile( e.target.files[ 0 ] ) } type="file" className='px-4' />
+                        <input onChange={ ( e ) => setFile( e.target.files[ 0 ] ) } type="file" className='px-4' />
 
-                    <div className='flex flex-col'>
-                        <div className='flex items-center space-x-4 md:space-x-8'>
+                        <div className='flex flex-col'>
+                            <div className='flex items-center space-x-4 md:space-x-8'>
 
-                            <input value={ cat } onChange={ ( e ) => setCat( e.target.value ) }  className='px-4 py-2 border-2 rounded-full' placeholder='Enter post category' type="text" />
+                                <input value={ cat } onChange={ ( e ) => setCat( e.target.value ) } className='px-4 py-2 border-2 rounded-full' placeholder='Enter post category' type="text" />
 
-                            <div onClick={ addCategory } className='bg-black hover:bg-gray-900  text-white px-4 py-2 font-semibold cursor-pointer '>Add</div>
+                                <div onClick={ addCategory } className='bg-black hover:bg-gray-900  text-white px-4 py-2 font-semibold cursor-pointer '>Add</div>
+                            </div>
+
+                            {/* categories */ }
+                            <div className='flex px-4 mt-3'>
+                                { cats?.map( ( c, i ) => (
+                                    <div key={ i } className='flex justify-center items-center space-x-2 mr-4 bg-gray-200 px-2 py-1 rounded-md'>
+                                        <p>{ c }</p>
+                                        <p onClick={ () => deleteCategory( i ) } className='text-white bg-black rounded-full cursor-pointer p-1 text-sm'><ImCross /></p>
+                                    </div>
+                                ) ) }
+
+
+                            </div>
                         </div>
 
-                        {/* categories */ }
-                        <div className='flex px-4 mt-3'>
-                            { cats?.map( ( c, i ) => (
-                                <div key={ i } className='flex justify-center items-center space-x-2 mr-4 bg-gray-200 px-2 py-1 rounded-md'>
-                                    <p>{ c }</p>
-                                    <p onClick={ () => deleteCategory( i ) } className='text-white bg-black rounded-full cursor-pointer p-1 text-sm'><ImCross /></p>
-                                </div>
-                            ) ) }
+                        <textarea onChange={ ( e ) => setDesc( e.target.value ) } rows={ 15 } cols={ 30 } className='px-4 py-2 rounded-2xl border-2' placeholder='Enter post description' />
 
-
-                        </div>
-                    </div>
-
-                    <textarea onChange={ ( e ) => setDesc( e.target.value ) } rows={ 15 } cols={ 30 }  className='px-4 py-2 rounded-2xl border-2'  placeholder='Enter post description' />
-
-                    <button onClick={ handleCreate } className='bg-black w-full md:w-[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg border-2 border-blue-800 hover:bg-gray-900 hover:rounded-full transition-all duration-500'>Create</button>
-                </form>
+                        <button onClick={ handleCreate } className='bg-black w-full md:w-[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg border-2 border-blue-800 hover:bg-gray-900 hover:rounded-full transition-all duration-500'>Create</button>
+                    </form>
 
                 </div>
-               
+
             </div>
             <Footer />
         </div>
