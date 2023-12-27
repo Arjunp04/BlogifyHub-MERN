485f7f4b69dc7b2c40f5c402600086c09a264a47
Arjunp04 <arjunprajapati4102001@gmail.com>
2023-12-27 15:30:35 +0530

    update verifytoken

--- backend/verifyToken.js
+++ backend/verifyToken.js
@@ -6,12 +6,12 @@ dotenv.config();
 const verifyToken = (req, res, next) => {
 
   const token = req.cookies.token;
-  console.log(token);
+  // console.log(token);
 
   if (!token) {
     return res.status(401).json("You are not authenticated!");
   }
-  
+
   jwt.verify(token, process.env.SECRET, async (err, data) => {
     if (err) {
       return res.status(403).json("Token is not valid!");
@@ -19,7 +19,7 @@ const verifyToken = (req, res, next) => {
 
     req.userId = data._id;
 
-    console.log("passed");
+    // console.log("passed");
 
     next();
   });
